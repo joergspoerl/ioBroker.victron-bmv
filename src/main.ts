3c2e639a5e3a470d8ae9fa5c79c00568aef683d4
Joerg Spoerl <joerg.spoerl@gmx.de>
2022-01-11 10:31:43 +0100

    remove unused config.options

--- src/main.ts
+++ src/main.ts
@@ -35,8 +35,6 @@ class victronBmv extends utils.Adapter {
 
 		// The adapters config (in the instance object everything under the attribute "native") is accessible via
 		// this.config:
-		this.log.info("config option1: " + this.config.option1);
-		this.log.info("config option2: " + this.config.option2);
 		this.log.info("config option2: " + this.config.serialPortPath);
 
 		/*
